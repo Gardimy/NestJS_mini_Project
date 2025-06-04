d023970fbd7c3bfeba831e99f22935f5d5a4b57d
Gardimy Charles <gardimycharles889@gmail.com>
2025-06-04 09:44:39 -0400

    Add users entity module, controller and services files

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,34 @@
+// src/users/users.service.ts
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(user: Partial<User>) {
+    return this.usersRepository.save(user);
+  }
+
+  findAll() {
+    return this.usersRepository.find();
+  }
+
+  findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+
+  async update(id: number, user: Partial<User>) {
+    await this.usersRepository.update(id, user);
+    return this.findOne(id);
+  }
+
+  remove(id: number) {
+    return this.usersRepository.delete(id);
+  }
+}
